d0794375e347b36e684dfe66aafddf792b6ba7d9
secustor <sebastian@poxhofer.at>
2024-02-26 20:53:55 +0100

    feat: add base Backstage instance

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,20 @@
+import { createBackend } from '@backstage/backend-defaults';
+
+const backend = createBackend();
+backend.add(import('@backstage/plugin-app-backend/alpha'));
+
+backend.add(import('@backstage/plugin-auth-backend'));
+
+backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+backend.add(import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'));
+backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'))
+
+backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
+
+backend.add(import('@backstage/plugin-search-backend/alpha'))
+backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'))
+backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
+
+backend.add(import('@backstage/plugin-techdocs-backend/alpha'))
+
+backend.start();
