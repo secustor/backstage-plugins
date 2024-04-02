b94087d8aba266299d2ffb3f032bc0d70535cf51
secustor <sebastian@poxhofer.at>
2024-04-02 20:18:15 +0200

    chore: fix linting issue

--- plugins/renovate-backend/src/index.ts
+++ plugins/renovate-backend/src/index.ts
@@ -1,6 +1,5 @@
 export * from './service/router';
 export { renovatePlugin as default } from './plugin';
-export * from './schema/renovate';
 export type { Context } from './service/types';
 export type * from './extensionPoints';
 export * from './extensionPoints';
