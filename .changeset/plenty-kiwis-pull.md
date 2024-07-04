2dca129a5d53a2b2afa8ea445e37a63feaabf139
secustor <sebastian@poxhofer.at>
2024-07-05 00:10:28 +0200

    feat(plugins): allow supplying URLs without protocol to /runs

---
+++ .changeset/plenty-kiwis-pull.md
@@ -0,0 +1,7 @@
+---
+'@secustor/backstage-plugin-renovate-backend': minor
+'@secustor/backstage-plugin-renovate-client': minor
+'@secustor/backstage-plugin-renovate-common': minor
+---
+
+Support supplying URLs with missing protocol to RenovateStarter
--- plugins/renovate-backend/src/service/schema.ts
+++ plugins/renovate-backend/src/service/schema.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
+import { targetRepo } from '@secustor/backstage-plugin-renovate-common';
 
-export const target = z.string();
+export const target = z.string().or(targetRepo);
 
 export const runRequestBody = z.object({
   target,
