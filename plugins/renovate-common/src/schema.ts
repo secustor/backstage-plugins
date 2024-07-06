b09452f056821beebcfbf9cd666868d6ac824fdb
secustor <sebastian@poxhofer.at>
2024-07-06 14:05:47 +0200

    fix(renovate): improve version logic and schema

--- plugins/renovate-common/src/schema.ts
+++ plugins/renovate-common/src/schema.ts
@@ -5,13 +5,58 @@ export const targetRepo = z.object({
   repository: z.string(),
 });
 
-export const problem = z.object({});
+export const problem = z.any();
 
-export const repositoryReport = z.object({
-  problems: z.array(problem),
-  branches: z.array(z.any()),
-  packageFiles: z.record(z.string(), z.any()),
-});
+export const update = z
+  .object({
+    updateType: z.string(),
+    newVersion: z.string().optional(),
+    lockedVersion: z.string().optional(),
+    newValue: z.string().nullish(),
+    newMajor: z.number().nullish(),
+    newMinor: z.number().nullish(),
+    newPatch: z.number().nullish(),
+  })
+  .passthrough();
+export type Update = z.infer<typeof update>;
+
+export const dependency = z
+  .object({
+    depName: z.string(),
+    datasource: z.string().nullish(),
+    depType: z.string().nullish(),
+    currentValue: z.string(),
+    currentVersion: z.string().nullish(),
+    updates: z.array(update).nullish(),
+    registryUrls: z.array(z.string()).nullish(),
+  })
+  .passthrough();
+
+export const packageFile = z
+  .object({
+    packageFile: z.string(),
+    packageFileVersion: z.string().nullish(),
+    deps: z.array(dependency),
+  })
+  .passthrough();
+
+export const branch = z
+  .object({
+    prNo: z.number().nullish(),
+    upgrades: z.array(z.any()),
+  })
+  .passthrough();
+
+export const packageFiles = z.record(z.string(), z.array(packageFile));
+export type PackageFiles = z.infer<typeof packageFiles>;
+
+export const repositoryReport = z
+  .object({
+    problems: z.array(problem),
+    branches: z.array(branch),
+    packageFiles,
+  })
+  .passthrough();
 export type RepositoryReport = z.infer<typeof repositoryReport>;
 
 export const renovateReport = z.object({
@@ -20,14 +65,16 @@ export const renovateReport = z.object({
 });
 export type RenovateReport = z.infer<typeof renovateReport>;
 
-export const repositoryReportResponseElement = z.object({
-  runID: z.string(),
-  taskID: z.string(),
-  timestamp: z.string().datetime(),
-  host: z.string(),
-  repository: z.string(),
-  report: repositoryReport,
-});
+export const repositoryReportResponseElement = z
+  .object({
+    runID: z.string(),
+    taskID: z.string(),
+    timestamp: z.string().datetime(),
+    host: z.string(),
+    repository: z.string(),
+    report: repositoryReport,
+  })
+  .passthrough();
 export type RepositoryReportResponseElement = z.infer<
   typeof repositoryReportResponseElement
 >;
