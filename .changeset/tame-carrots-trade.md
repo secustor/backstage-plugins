ae2c3e78a1f31fc33af1e04d5cebad652e054ced
secustor <sebastian@poxhofer.at>
2024-07-10 09:40:57 +0200

    feat(ReportFetchComponent): add an empty state and add a scan button

---
+++ .changeset/tame-carrots-trade.md
@@ -0,0 +1,5 @@
+---
+'@secustor/backstage-plugin-renovate': patch
+---
+
+Add Empty state to RenovateEntityContent with rescan functionality
--- plugins/renovate/src/components/RenovateStarter/RenovateStarter.tsx
+++ plugins/renovate/src/components/RenovateStarter/RenovateStarter.tsx
@@ -1,44 +1,12 @@
 import { InfoCard } from '@backstage/core-components';
-import {
-  Button,
-  CardActions,
-  CircularProgress,
-  TextField,
-} from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 import React, { useState } from 'react';
-import { useApi } from '@backstage/core-plugin-api';
-import { renovateApiRef } from '../../api';
-import is from '@sindresorhus/is';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import { StartRenovateButton } from '../StartRenovateButton/StartRenovateButton';
 
 export const RenovateStarter = () => {
-  const renovateAPI = useApi(renovateApiRef);
   const [repoURL, setRepoURL] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
   const [inputError, setInputError] = useState<boolean>(false);
 
-  const timer = React.useRef<number>();
-
-  const triggerRenovateRun = async () => {
-    if (is.emptyString(repoURL)) {
-      setInputError(true);
-      return;
-    }
-
-    setInputError(false);
-    setLoading(true);
-
-    await renovateAPI.runsPost({
-      body: {
-        target: repoURL,
-      },
-    });
-
-    // add some sleep, so it is actually visible that we have done something
-    timer.current = window.setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  };
   return (
     <InfoCard title="Run Renovate">
       <TextField
@@ -50,18 +18,11 @@ export const RenovateStarter = () => {
         onChange={e => setRepoURL(e.target.value)}
         margin="normal"
       />
-      <CardActions>
-        {loading && <CircularProgress />}
-        <Button
-          id="run-renovate"
-          variant="contained"
-          endIcon={<PlayArrowIcon />}
-          onClick={triggerRenovateRun}
-          disabled={loading}
-        >
-          Run Renovate
-        </Button>
-      </CardActions>
+      <StartRenovateButton
+        target={repoURL}
+        onFailure={() => setInputError(true)}
+        onSuccess={() => setInputError(false)}
+      />
     </InfoCard>
   );
 };
