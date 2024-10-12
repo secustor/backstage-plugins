954bdb0bed413ef086b83bca7cd6dd477f878f76
Sebastian Poxhofer <secustor@users.noreply.github.com>
2024-10-13 01:14:58 +0200

    Fix/pagination model (#405)

--- plugins/renovate/src/components/DependencyTable/defaultColumns.tsx
+++ plugins/renovate/src/components/DependencyTable/defaultColumns.tsx
@@ -4,6 +4,20 @@ import type { FilterableColumnDef } from './types';
 
 export const defaultColumns: FilterableColumnDef[] = [
   {
+    headerName: 'ID',
+    field: 'id',
+    isHiddenOnInit: true,
+    // no available data endpoint for this field
+    isFilterable: false,
+  },
+  {
+    headerName: 'Run ID',
+    field: 'runID',
+    isHiddenOnInit: true,
+    // no available data endpoint for this field
+    isFilterable: false,
+  },
+  {
     headerName: 'Host',
     field: 'host',
     flex: 0.5,
@@ -58,12 +72,35 @@ export const defaultColumns: FilterableColumnDef[] = [
     headerName: 'Current Value',
     field: 'currentValue',
     flex: 1,
+    // no available data endpoint for this field
     isFilterable: false,
   },
   {
     headerName: 'Skip Reason',
     field: 'skipReason',
     flex: 1,
+    // no available data endpoint for this field
     isFilterable: false,
+  },
+  {
+    headerName: 'Registry URL',
+    field: 'registryUrl',
+    isHiddenOnInit: true,
+    // no available data endpoint for this field
+    isFilterable: false,
+  },
+  {
+    headerName: 'Source URL',
+    field: 'sourceUrl',
+    isHiddenOnInit: true,
+    // no available data endpoint for this field
+    isFilterable: false,
+    renderCell: ({ row }) => {
+      const sourceUrl = row.sourceUrl;
+      if (sourceUrl) {
+        return <Link to={sourceUrl}>{sourceUrl}</Link>;
+      }
+      return null;
+    },
   },
 ];
