777a91b9e3b6a29f12c56c2892215886354f03d4
secustor <sebastian@poxhofer.at>
2025-02-14 21:52:15 +0100

    fix(renovate-backend): handle Renovate log objects correctly

---
+++ .changeset/tall-stingrays-hear.md
@@ -0,0 +1,5 @@
+---
+'@secustor/backstage-plugin-renovate-backend': patch
+---
+
+Forward log objects of Renovate as JSON strings to Backstage. This will correctly render Renovate errors and config objects in log lines.
--- plugins/renovate-backend/src/wrapper/utils.test.ts
+++ plugins/renovate-backend/src/wrapper/utils.test.ts
@@ -1,9 +1,113 @@
-import { getCacheEnvs } from './utils';
+import { extractReport, getCacheEnvs } from './utils';
 import { mockServices } from '@backstage/backend-test-utils';
 import { MockConfigApi } from '@backstage/test-utils';
+import { PassThrough } from 'stream';
+import { RenovateReport } from '@secustor/backstage-plugin-renovate-common';
 
 describe('renovate-backend/wrapper/utils', () => {
   const logger = mockServices.logger.mock();
+
+  describe('extractReport', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it('extracts report', async () => {
+      const report: RenovateReport = {
+        problems: [],
+        repositories: {
+          'a-repository': {
+            problems: [],
+            branches: [],
+            packageFiles: {
+              'a-manager': [],
+            },
+          },
+        },
+      };
+
+      const logStream = new PassThrough();
+      logStream.write(`${JSON.stringify({ report })}\n`);
+      logStream.end();
+      await expect(
+        extractReport({
+          logger,
+          logStream,
+        }),
+      ).resolves.toEqual(report);
+
+      expect(logger.debug).not.toHaveBeenCalled();
+    });
+
+    it('extracts report in between lines', async () => {
+      const report: RenovateReport = {
+        problems: [],
+        repositories: {
+          'a-repository': {
+            problems: [],
+            branches: [],
+            packageFiles: {
+              'a-manager': [],
+            },
+          },
+        },
+      };
+
+      const logStream = new PassThrough();
+      logStream.write(`${JSON.stringify({ msg: 'Foo' })}\n`);
+      logStream.write(
+        `${JSON.stringify({ msg: 'Printing report', report })}\n`,
+      );
+      logStream.write(`${JSON.stringify({ msg: 'Bar' })}\n`);
+      logStream.end();
+      await expect(
+        extractReport({
+          logger,
+          logStream,
+        }),
+      ).resolves.toEqual(report);
+    });
+
+    it('forward Renovate log metadata to Backstage logger service', async () => {
+      const report: RenovateReport = {
+        problems: [],
+        repositories: {
+          'a-repository': {
+            problems: [],
+            branches: [],
+            packageFiles: {
+              'a-manager': [],
+            },
+          },
+        },
+      };
+
+      const logStream = new PassThrough();
+      logStream.write(
+        `${JSON.stringify({ msg: 'Message foo', foo: 'bar' })}\n`,
+      );
+      logStream.write(
+        `${JSON.stringify({ msg: 'Printing report', report })}\n`,
+      );
+      logStream.write(
+        `${JSON.stringify({ msg: 'Message bar', bar: 'foo' })}\n`,
+      );
+      logStream.end();
+      await expect(
+        extractReport({
+          logger,
+          logStream,
+        }),
+      ).resolves.toEqual(report);
+      expect(logger.debug).toHaveBeenNthCalledWith(1, 'Message foo', {
+        foo: '"bar"',
+      });
+      expect(logger.debug).toHaveBeenNthCalledWith(2, 'Message bar', {
+        bar: '"foo"',
+      });
+    });
+  });
+
   describe('getCacheEnvs', () => {
     it('returns empty for empty', () => {
       const config = new MockConfigApi({
