d12ba83342738919489c8519ac7c03f90e09144b
secustor <sebastian@poxhofer.at>
2025-04-03 22:28:13 +0100

    fixup

--- plugins/renovate/dev/index.tsx
+++ plugins/renovate/dev/index.tsx
@@ -1,6 +1,6 @@
 import { createApp } from '@backstage/frontend-defaults';
 import RenovatePlugin from '../src/alpha.tsx';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 const app = createApp({
   features: [RenovatePlugin],
