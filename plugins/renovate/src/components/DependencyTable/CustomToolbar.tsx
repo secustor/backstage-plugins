87f1b4eb4ab9e6e847b26d19773245ac60a3814a
secustor <sebastian@poxhofer.at>
2025-05-11 12:29:59 +0200

    chore: migrate components

--- plugins/renovate/src/components/DependencyTable/CustomToolbar.tsx
+++ plugins/renovate/src/components/DependencyTable/CustomToolbar.tsx
@@ -1,10 +1,12 @@
 import {
   GridToolbarColumnsButton,
-  GridToolbarContainer,
-  GridToolbarExport,
-  GridToolbarQuickFilter,
+  QuickFilter,
+  Toolbar,
   GridFilterListIcon,
   GridSlotsComponentsProps,
+  QuickFilterControl,
+  QuickFilterClear,
+  ExportCsv,
 } from '@mui/x-data-grid';
 import { useState } from 'react';
 import Box from '@mui/material/Box';
@@ -12,6 +14,13 @@ import Button from '@mui/material/Button';
 import { Filters } from './Filters';
 import type { FilterableColumnDef } from './types';
 import Dialog from '@mui/material/Dialog';
+import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+import CancelIcon from '@mui/icons-material/Cancel';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import Tooltip from '@mui/material/Tooltip';
 
 declare module '@mui/x-data-grid' {
   interface ToolbarPropsOverrides {
@@ -20,6 +29,10 @@ declare module '@mui/x-data-grid' {
     filterAbleColumns?: FilterableColumnDef[];
   }
 }
+
+const StyledQuickFilter = styled(QuickFilter)({
+  marginLeft: 'auto',
+});
 
 export function CustomToolbar(
   props: NonNullable<GridSlotsComponentsProps['toolbar']>,
@@ -31,7 +44,7 @@ export function CustomToolbar(
     [];
 
   return (
-    <GridToolbarContainer>
+    <Toolbar>
       <Button
         title="Open filter drawer"
         startIcon={<GridFilterListIcon />}
@@ -52,14 +65,55 @@ export function CustomToolbar(
 
       <GridToolbarColumnsButton />
       <Box sx={{ flexGrow: 1 }} />
-      <GridToolbarQuickFilter variant="standard" />
-      <GridToolbarExport
-        slotProps={{
-          tooltip: { title: 'Export data' },
-          button: { variant: 'outlined' },
-        }}
-      />
-    </GridToolbarContainer>
+      <StyledQuickFilter expanded>
+        <QuickFilterControl
+          render={({ ref, ...other }) => (
+            <TextField
+              {...other}
+              sx={{ width: 260 }}
+              inputRef={ref}
+              aria-label="Search"
+              placeholder="Search..."
+              size="small"
+              slotProps={{
+                input: {
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SearchIcon fontSize="small" />
+                    </InputAdornment>
+                  ),
+                  endAdornment: other.value ? (
+                    <InputAdornment position="end">
+                      <QuickFilterClear
+                        edge="end"
+                        size="small"
+                        aria-label="Clear search"
+                      >
+                        <CancelIcon fontSize="small" />
+                      </QuickFilterClear>
+                    </InputAdornment>
+                  ) : null,
+                  ...other.slotProps?.input,
+                },
+                ...other.slotProps,
+              }}
+            />
+          )}
+        />
+      </StyledQuickFilter>
+      <Tooltip title="Download as CSV">
+        <ExportCsv
+          options={{
+            fileName: 'dependencies',
+          }}
+          render={
+            <Button startIcon={<FileDownloadIcon fontSize="small" />}>
+              EXPORT
+            </Button>
+          }
+        />
+      </Tooltip>
+    </Toolbar>
   );
 }
 
--- plugins/renovate/src/components/RenovateDefaultOverview/RenovateDefaultOverview.tsx
+++ plugins/renovate/src/components/RenovateDefaultOverview/RenovateDefaultOverview.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react';
-import Grid from '@mui/material/Grid2';
+import Grid from '@mui/material/Grid';
 import {
   Header,
   Page,
